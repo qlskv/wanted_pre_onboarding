5943a88d697c59ef2f6083cc72334ec283c1813b
qlskv <htkv582@naver.com>
2022-09-12 12:31:03 +0900

    first commit

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by AirM2 on 2022/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
